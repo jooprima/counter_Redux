db6c726af124a8bacbfee0c51d3c445b60c3ed55
jooprima <jooprima@gmail.com>
2019-09-29 23:40:24 +0700

    create fungsi increment di reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,19 @@ import { createStore } from "redux";
 
 const initialState = {
   count: 5,
-  x :0
+  x: 0
 };
 
 const reducer = (state = initialState, action) => {
   console.log("reducer berjalan", action);
-  return state;
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case "INCREMENT":
+      return Object.assign({}, state, { count: state.count + 1 });
+    default:
+      return state;
+  }
+//   return state;
 };
 
 const store = createStore(reducer);
